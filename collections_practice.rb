b68fba0e5d4b7f05d3e74298e7750118b381c568
Ursula Miller <ursula.miller@gmail.com>
2019-06-26 05:34:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,6 +12,8 @@ def sort_array_char_count(strings)
   strings.sort {|a,b| a.length > b.length ? a <=>b : b <=>a }  
 end
 
+
+###### doesn't work
 def swap_elements(items)
  # items[0], items[1], items[2] = items[0], items[2], items[1]
  items.collect.swap!(1,2)
@@ -20,7 +22,7 @@ end
 def swap_elements_from_to(array, index, destination_index)
   array[index] = array[destination_index]  
 end
-
+###### and the bonus doesn't work either
 
 def reverse_array(integers)
   integers.reverse
